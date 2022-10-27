3a2247b5a698a7a0adb0c9bf9e19f104195e740f
Vigero158 <vigero158@gmail.com>
2022-10-27 14:43:33 -0400

    Adding code

---
+++ src/User.java
@@ -0,0 +1,37 @@
+public class User {
+    private int userID;
+    private String userName;
+    private String passwordHash;
+    private String passwordSalt;
+    private String email;
+    private int CardInfo;
+    //private Ticket tickets[];
+
+    private User(String userName, String email, String password){
+        this.userName = userName;
+        this.email = email;
+        if (verifyPassword(password)) {
+            System.out.println("The cake is a lie.");
+        } else {
+            System.out.println("Wrong password.");
+        }
+    }
+
+    private static boolean verifyPassword (String password){
+        // TODO: Write password verification function.
+        return false;
+    }
+
+    private static void passwordHashing(String password){
+        // TODO: Write a password hashing function.
+    }
+
+    public static String searchForEvent(){
+        // TODO: User input.
+        return "This is a search";
+    }
+
+    public static void purchaseTicket(){
+        // TODO: Write function to purchase a ticket (using fake information);
+    }
+}
